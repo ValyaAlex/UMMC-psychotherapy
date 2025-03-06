a4ea2dfcc7ac35eebdbd5477729b786a950533a6
Valentina <afonasovava@mail.ru>
2025-03-06 14:31:20 +0500

    added comment

--- js/burger.js
+++ js/burger.js
@@ -6,7 +6,7 @@ let headerBg = document.querySelector(".header");
 
 burger.addEventListener("click", function () {
   burger.classList.toggle("burger--active");
-  headerBg.classList.toggle("header-bg");
+  headerBg.classList.toggle("header-bg");  // добавляется класс к хедеру
   menu.classList.toggle("nav--active");
   document.body.classList.toggle("stop-scroll");
 });
@@ -14,18 +14,18 @@ burger.addEventListener("click", function () {
 menuLinks.forEach(function (el) {
   el.addEventListener("click", function () {
     burger.classList.remove("burger--active");
-    headerBg.classList.remove("header-bg");
+    headerBg.classList.remove("header-bg"); // убирается класс у хедера
     menu.classList.remove("nav--active");
     document.body.classList.remove("stop-scroll");
   });
 });
 
-responsiveOverlay.addEventListener("click", (e) => {
+responsiveOverlay.addEventListener("click", (e) => { // в выложенной версии лендинга на данный момент меню не закрывается при нажатии на пустое поле
   if (e.target == responsiveOverlay) {
     menu.classList.remove("nav--active");
     document.body.classList.remove("stop-scroll");
     burger.classList.remove("burger--active");
-    headerBg.classList.remove("header-bg");
+    headerBg.classList.remove("header-bg");// убирается класс у хедера
   }
 });
 
